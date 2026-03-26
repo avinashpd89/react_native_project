a3cd65b9ff253e7fa93c798b46892bf0073e0d74
Avinash Prasad <avinashprasad4608@gmail.com>
2026-03-26 12:24:40 +0530

    add new project Spotifymusic

---
+++ Spotifymusic/src/App.tsx
@@ -0,0 +1,57 @@
+import React, {useState, useEffect} from "react"
+
+import type {JSX} from 'react';
+import {
+  ActivityIndicator,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
+import {
+  SafeAreaView,
+} from 'react-native-safe-area-context';
+
+import {setupPlayer, addTrack} from "../musicPlayerServices.js"
+import MusicPlayer from "./screens/MusicPlayer";
+
+
+function App(): JSX.Element {
+  const [isPlayerReady, setIsPaylerReady] = useState(false)
+
+  async function setup(){
+    let isSetup = await setupPlayer()
+
+    if (isSetup) {
+      await addTrack()
+    }
+
+    setIsPaylerReady(isSetup)
+  }
+
+  useEffect(() => {
+    setup()
+  }, [])
+  
+  if (!isPlayerReady) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={"light-content"} />
+      <MusicPlayer />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1
+  }
+});
+
+export default App;
